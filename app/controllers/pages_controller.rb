7709453ee8e90d97e5977176895092ed524cf47c
tomka88 <kocsis.tamas88@gmail.com>
2015-09-15 14:14:50 +0200

    Added basic sub-pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def rolunk
+  end
+
+  def kapcsolat
+  end
+
+  def arajanlat
+  end
+
+  def szolgaltatasaink
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get "rolunk" => "pages#rolunk"
+  get "kapcsolat" => "pages#kapcsolat"
+  get "arajanlat" => "pages#arajanlat"
+  get "szolgaltatasaink" => "pages#szolgaltatasaink"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
